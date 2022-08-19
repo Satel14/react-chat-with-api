cd0745c62cc1af57d24302731a2fc7320931e3e9
Satel14 <ostaplvov@gmail.com>
2022-08-19 15:42:05 +0300

    add localstore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { store } from './redux/store';
+import { store, persist} from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 
-
-  ReactDOM.render(
-      <Provider store={store}>
-          <App />
-      </Provider>,
+ReactDOM.render(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persist}>
+            <App />
+        </PersistGate >
+    </Provider>,
     document.getElementById('root')
-  );
+);
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,20 @@ import dialogsReducer from "../redux/reducers/dialogReducer";
 import thunkMiddleWare from 'redux-thunk';
 import usersReducer from "./reducers/usersReducer";
 import authReducer from "./reducers/authReducer";
+import storage from 'redux-persist/lib/storage' 
+import { persistStore, persistReducer } from 'redux-persist'
 
-
-
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
 
 const reducers = combineReducers({
     dialogs: dialogsReducer,
     users: usersReducer,
     auth: authReducer
 });
+const persistedReducer = persistReducer(persistConfig, reducers);
 
-export const store  = createStore(reducers, applyMiddleware(thunkMiddleWare));
+export const store  = createStore(persistedReducer, applyMiddleware(thunkMiddleWare));
+export const persist = persistStore(store)
